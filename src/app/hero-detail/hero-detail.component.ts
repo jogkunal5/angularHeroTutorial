27a237075f245d6629d8093e3b05c373064832a4
jogkunal5 <jogkunal5@gmail.com>
2019-04-28 11:37:58 +0530

    Latest changs in angular hero tutorial

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  //The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent. 
+  //This component is interested in the route's bag of parameters extracted from the URL. The "id" parameter is the id of the hero to display.
+
+  //The location is an Angular service for interacting with the browser. You'll use it later to navigate back to the view that navigated here.
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,26 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    { id: 1, name: 'Tony Stark - Ironman' },
+    { id: 2, name: 'Steve Rogers - Captain America' },
+    { id: 3, name: 'Thor - God of Thunder' },
+    { id: 4, name: 'Carol Danvers - Captain Marvel' },
+    { id: 5, name: 'Bruce Banner - Hulk' },
+    { id: 6, name: 'Scott Lang - Antman' },    
+    { id: 7, name: 'Dr. Stephen Strange - Dr. Strange' },
+    { id: 8, name: 'T-Challa - Black Panther' },
+    { id: 9, name: 'Natasha - Black Widow' },    
+    { id: 10, name: 'Clinton Barton - Hawkaye' },    
+    { id: 11, name: 'Peter Parker - Spiderman' },
+    { id: 12, name: 'Loki - God of Mischief' },
+    { id: 13, name: 'Peter Quill - Star Lord' },
+    { id: 14, name: 'Rocket Raccoon - Rabbit' },
+    { id: 15, name: 'Groot - I am Groot' },
+    { id: 16, name: 'Scarlet Witch - Wanda' },
+    { id: 17, name: 'Nebula' },
+    { id: 18, name: 'Gamora' },
+    { id: 19, name: 'Drax' },
+    { id: 20, name: 'James Rhoes - War Machine' },
+    { id: 21, name: 'Nick Fury' },
+    { id: 22, name: 'Vision - Altron' },
+]
